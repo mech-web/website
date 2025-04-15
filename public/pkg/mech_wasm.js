1616ed80a343ce83c1410326aca90fddc7301c27
cmontella <cmontella@live.com>
2025-04-14 23:38:40 -0400

    Update post

--- public/pkg/mech_wasm.js
+++ public/pkg/mech_wasm.js
@@ -135,7 +135,7 @@ export function main() {
 }
 
 function __wbg_adapter_14(arg0, arg1) {
-    wasm._dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h096fb76d5914925b(arg0, arg1);
+    wasm._dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h563fb7515b86494a(arg0, arg1);
 }
 
 const WasmMechFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -274,8 +274,8 @@ function __wbg_get_imports() {
         const ret = typeof window === 'undefined' ? null : window;
         return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper327 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 102, __wbg_adapter_14);
+    imports.wbg.__wbindgen_closure_wrapper332 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 104, __wbg_adapter_14);
         return ret;
     };
     imports.wbg.__wbindgen_init_externref_table = function() {
